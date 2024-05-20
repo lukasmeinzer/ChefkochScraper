f4d39002e19e19e8910ce8c620af7a138f9ba83f
lukas <lukas.meinzer.98@web.de>
2024-05-20 08:05:51 +0000

    Anpassungen in main und schreiben in DB

--- recipe_aquire/main.py
+++ recipe_aquire/main.py
@@ -1,28 +1,20 @@
 from chefkoch import ChefKochAPI, DataParser
+from db_inserter import Tabellarisieren, table_into_db
 import os
 
 if __name__ == '__main__':
-    categories = ChefKochAPI.get_categories()
-
-    # recipe_amount = 200
-    # recipes = []
-    category = categories[0]
     os.makedirs("Rezepte", exist_ok=True)
+    
+    categories = ChefKochAPI.get_categories()
+    
     for category in categories[0:]:
-        if category.title == "Punsch Rezepte":
-            category_recipes = ChefKochAPI.parse_recipes(category)
-            path = "Rezepte/" + category.title.replace(" ", "-").replace("/", "-")
-            DataParser.write_recipes_to_json(
-                file_path=path,
-                recipes=category_recipes,
-                min_rating=5
-                )
-                
-                
-                
-import json
-
-file_path = "test.json"
-with open(file_path) as file:
-    raw_text = file.read()
-content = json.loads(raw_text)
+        category_recipes = ChefKochAPI.parse_recipes(category)
+        path = "Rezepte/" + category.title.replace(" ", "-").replace("/", "-")
+        DataParser.write_recipes_to_json(
+            file_path=path,
+            recipes=category_recipes
+            )
+            
+    # In DB schreiben
+    df = Tabellarisieren(categories) 
+    table_into_db(df)
